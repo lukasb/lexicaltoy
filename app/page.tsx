575b47da407823598e29b92f9e55bec7c4d08d24
lukasb <lukasb@gmail.com>
2024-01-29 14:06:16 -0800

    styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="relative w-full h-96">
-        <div className="border-solid border-2 border-indigo-600 m-4 p-5 w-full">
+        <div className="border-solid border-4 border-indigo-300 rounded-lg m-4 p-5 w-full max-w-7xl">
           <Editor />
         </div>
       </div>
