7421b50e16684152118355c7f5d0cf1422b01999
lukasb <lukasb@gmail.com>
2024-02-01 19:18:20 -0800

    cleanup script works

---
+++ tests/cleanup-db-tests.js
@@ -0,0 +1,46 @@
+require('dotenv').config({ path: './.env.local' }); 
+const { db } = require('@vercel/postgres');
+const {
+  users,
+  pages
+} = require('./tests-placeholder-data.js');
+
+async function main() {
+  const client = await db.connect();
+  await cleanUp(client, users);
+  await client.end();
+}
+
+async function cleanUp(client, users) {
+  try {
+    const deletedUsers = await Promise.all(
+      users.map(async (user) => {
+        return client.sql`
+        DELETE FROM users
+        WHERE id = ${user.id};
+      `;
+      }),
+    );
+
+    const deletedPages = await Promise.all(
+      users.map(async (user) => {
+        return client.sql`
+        DELETE FROM pages
+        WHERE userId = ${user.id};
+      `;
+      }),
+    );
+
+    console.log(`Cleaned up db`);
+
+    return {
+      deletedUsers,
+      deletedPages
+    };
+  } catch (error) {
+    console.error('Error cleaning up:', error);
+    throw error;
+  }
+}
+
+main();
