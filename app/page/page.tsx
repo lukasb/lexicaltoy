d7aefd6c23eac7e2c9d1de6635fdea033592cdec
lukasb <lukasb@gmail.com>
2024-02-03 19:31:29 -0800

    mobile styling fix

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
     <div className="flex justify-center items-center">
       <div className="relative w-full">
       <div className="flex flex-col items-start">
-        <div className="border-solid border-4 border-indigo-300 rounded-lg m-4 p-7 w-full max-w-7xl">
+        <div className="border-solid border-4 border-indigo-300 rounded-lg m-0 md:m-4 p-7 w-full max-w-7xl">
           <Editor
             initialPageContent={initialPageContent}
             pageId={pageId}
