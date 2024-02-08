707dc2eb3a337551d85948a3cb98c700961b8a5b
lukasb <lukasb@gmail.com>
2024-02-08 14:55:03 -0800

    don't re-open an already open page

--- app/page/EditingArea.tsx
+++ app/page/EditingArea.tsx
@@ -9,16 +9,10 @@ import { insertPage } from "../lib/actions";
 import { useEffect, useRef } from "react";
 import { Button } from "../ui/button";
 
-function EditingArea({
-  pages,
-  userId
-}: {
-  pages: Page[];
-  userId: string;
-}) {
-
+function EditingArea({ pages, userId }: { pages: Page[]; userId: string }) {
   const [currentPages, setCurrentPages] = useState(pages);
-  const emptyPageJSONString = '{"root":{"children":[{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"listitem","version":1,"value":1}],"direction":null,"format":"","indent":0,"type":"list","version":1,"listType":"bullet","start":1,"tag":"ul"}],"direction":null,"format":"","indent":0,"type":"root","version":1}}';
+  const emptyPageJSONString =
+    '{"root":{"children":[{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"listitem","version":1,"value":1}],"direction":null,"format":"","indent":0,"type":"list","version":1,"listType":"bullet","start":1,"tag":"ul"}],"direction":null,"format":"","indent":0,"type":"root","version":1}}';
 
   // TODO let findMostRecentlyEditedPage return null if no pages
   // then create a new page if no pages
@@ -29,17 +23,17 @@ function EditingArea({
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.metaKey && event.key === 'k') {
+      if (event.metaKey && event.key === "k") {
         event.preventDefault();
         omnibarRef.current?.focus();
       }
     };
 
-    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener("keydown", handleKeyDown);
 
     // Clean up the event listener when the component unmounts
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener("keydown", handleKeyDown);
     };
   }, []);
 
@@ -47,13 +41,16 @@ function EditingArea({
     if (!currentPages.includes(page)) {
       setCurrentPages([...currentPages, page]);
     }
-    if (!openPages.includes(page)) {
-      setOpenPages(prevPages => [page, ...prevPages]);
+    const pageIndex = openPages.findIndex((p) => p.id === page.id);
+    if (pageIndex === -1) {
+      setOpenPages((prevPages) => [page, ...prevPages]);
     } else {
-      // move page to the beginning of the array
-      setOpenPages(prevPages => [page, ...prevPages.filter(p => p !== page)]);
+      setOpenPages((prevPages) => {
+        const otherPages = prevPages.filter((p) => p.id !== page.id);
+        return [page, ...otherPages];
+      });
     }
-  }
+  };
 
   const handleNewPage = async (title: string) => {
     const result = await insertPage(title, emptyPageJSONString, userId);
@@ -63,47 +60,49 @@ function EditingArea({
     } else if (isPage(result)) {
       openPage(result);
     }
-  }
+  };
 
   return (
     <div className="md:p-4 lg:p-10 xl:p-20 2xl:p-30 transition-spacing ease-linear duration-75">
       <Omnibar
-        ref={omnibarRef} 
-        pages={currentPages} 
+        ref={omnibarRef}
+        pages={currentPages}
         createNewPage={(title) => handleNewPage(title)}
         openPage={openPage}
       />
       {openPages.length === 0 ? (
         <div className="w-full h-40 flex justify-center items-center">
-        <Button 
-          onClick={() => handleNewPage('New Page')}
-        >
-          Create New Page
-        </Button>
+          <Button onClick={() => handleNewPage("New Page")}>
+            Create New Page
+          </Button>
         </div>
       ) : (
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-        {openPages.map(page => (
-          <EditorContainer
-            key={page.id}
-            pageId={page.id}
-            initialPagetitle={page.title}
-            initialPageContent={page.value}
-            updatePageTitleLocal={(id, newTitle) => {
-              setCurrentPages(currentPages.map(page => 
-                page.id === id ? { ...page, title: newTitle } : page
-              ));
-            }}
-            updatePageContentsLocal={(id, newValue) => {
-              setCurrentPages(currentPages.map(page => 
-                page.id === id ? { ...page, value: newValue } : page
-              ));
-            }}
-            closePage={(id) => {
-              setOpenPages(openPages.filter(page => page.id !== id));
-            }}
-          />
-        ))}
+          {openPages.map((page) => (
+            <EditorContainer
+              key={page.id}
+              pageId={page.id}
+              initialPagetitle={page.title}
+              initialPageContent={page.value}
+              updatePageTitleLocal={(id, newTitle) => {
+                setCurrentPages(
+                  currentPages.map((page) =>
+                    page.id === id ? { ...page, title: newTitle } : page
+                  )
+                );
+              }}
+              updatePageContentsLocal={(id, newValue) => {
+                setCurrentPages(
+                  currentPages.map((page) =>
+                    page.id === id ? { ...page, value: newValue } : page
+                  )
+                );
+              }}
+              closePage={(id) => {
+                setOpenPages(openPages.filter((page) => page.id !== id));
+              }}
+            />
+          ))}
         </div>
       )}
     </div>
