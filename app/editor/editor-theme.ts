4ffaa88e3d299c0bfd5b6650fc27e2613ead9267
lukasb <lukasb@gmail.com>
2024-02-12 15:38:10 -0800

    styling for wikilinks

--- app/editor/editor-theme.ts
+++ app/editor/editor-theme.ts
@@ -2,6 +2,7 @@ export const theme = {
     blockCursor: 'PlaygroundEditorTheme__blockCursor',
     indent: 'PlaygroundEditorTheme__indent',
     link: 'PlaygroundEditorTheme__link',
+    wikilink: 'PlaygroundEditorTheme__wikilink',
     list: {
         listitem: 'PlaygroundEditorTheme__listItem',
         nested: {
