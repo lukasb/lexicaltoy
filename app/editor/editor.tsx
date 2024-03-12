53cb8611137e622c52358a9e27f87e7a0e02fe86
lukasb <lukasb@gmail.com>
2024-03-11 21:04:41 -0700

    get rid of some logging

--- app/editor/editor.tsx
+++ app/editor/editor.tsx
@@ -97,10 +97,6 @@ function Editor({
     }
   };
 
-  useEffect(() => {
-    console.log("re-rendering editor", page.title);
-  });
-
   // TODO this assumes the page content won't be changed elsewhere in the same PagesContext
   const storePage = useDebouncedCallback(async (outline) => {
     console.log("Storing page");
