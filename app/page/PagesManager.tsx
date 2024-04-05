5222cf4fcc6ae568ecd68d78f423e0dd354256e8
lukasb <lukasb@gmail.com>
2024-04-04 18:11:29 -0700

    debounce page saves

--- app/page/PagesManager.tsx
+++ app/page/PagesManager.tsx
@@ -3,38 +3,39 @@ import { PagesContext } from '@/app/context/pages-context';
 import { updatePageContentsWithHistory } from "../lib/actions";
 import { Page } from "@/app/lib/definitions";
 import { useSharedNodeContext } from '../context/shared-node-context';
+import { useDebouncedCallback } from "use-debounce";
 
 // TODO maybe use Redux so we don't have an O(n) operation here every time
 function PagesManager({ setPages }: { setPages: React.Dispatch<React.SetStateAction<Page[]>> }) {
   const pages = useContext(PagesContext);
   const { sharedNodeMap } = useSharedNodeContext();
 
-  useEffect(() => {
-    const savePagesToDatabase = async () => {
-      for (const page of pages) {
-        if (page.pendingWrite) {
-          console.log("Saving page to database", page.title);
-          try {
-            const newRevisionNumber = await updatePageContentsWithHistory(page.id, page.value, page.revisionNumber);
-            if (newRevisionNumber === -1) {
-              alert(`Failed to save page ${page.title} because you edited an old version, please relead for the latest version.`);
-              return;
-            }
-            // Update the pages context with the new revision number
-            setPages((prevPages) =>
-              prevPages.map((p) =>
-                p.id === page.id ? { ...p, pendingWrite: false, revisionNumber: newRevisionNumber } : p
-              )
-            );
-          } catch (error) {
-            alert("Failed to save page");
+  const savePagesToDatabase = useDebouncedCallback(async () => {
+    for (const page of pages) {
+      if (page.pendingWrite) {
+        console.log("Saving page to database", page.title);
+        try {
+          const newRevisionNumber = await updatePageContentsWithHistory(page.id, page.value, page.revisionNumber);
+          if (newRevisionNumber === -1) {
+            alert(`Failed to save page ${page.title} because you edited an old version, please relead for the latest version.`);
+            return;
           }
+          // Update the pages context with the new revision number
+          setPages((prevPages) =>
+            prevPages.map((p) =>
+              p.id === page.id ? { ...p, pendingWrite: false, revisionNumber: newRevisionNumber } : p
+            )
+          );
+        } catch (error) {
+          alert("Failed to save page");
         }
       }
-    };
+    }
+  }, 500);
 
+  useEffect(() => {
     savePagesToDatabase();
-  }, [pages, setPages]);
+  }, [pages, setPages, savePagesToDatabase]);
 
   // TODO maybe use Redux so we don't have an O(n) operation here every time
   useEffect(() => {
