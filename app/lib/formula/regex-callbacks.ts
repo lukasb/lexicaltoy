84a128e14fa150322982cfcddc80647ab34bbe12
lukasb <lukasb@gmail.com>
2024-04-24 12:13:11 -0700

    we can use the argument to find

--- app/lib/formula/regex-callbacks.ts
+++ app/lib/formula/regex-callbacks.ts
@@ -4,10 +4,15 @@ import {
 } from "./formula-definitions";
 
 export const regexCallbacks: Array<[RegExp, (match: RegExpMatchArray) => Promise<FormulaOutput>]> = [
-  [/^find\(\)$/, async () => (
-    { output: [
-      {nodeMarkdown: '- hello there', pageName: 'some page', lineNumber: 1},
-      {nodeMarkdown: '- general kenobi', pageName: 'another page', lineNumber: 2},
-      {nodeMarkdown: '- you are a bold one', pageName: 'another page', lineNumber: 3}
-    ], type: FormulaOutputType.NodeMarkdown })],
+  [/^find\((.+)\)$/, async (match) => {
+    console.log(match[1]);
+    return {
+      output: [
+        {nodeMarkdown: '- hello there', pageName: 'some page', lineNumber: 1},
+        {nodeMarkdown: '- general kenobi', pageName: 'another page', lineNumber: 2},
+        {nodeMarkdown: '- you are a bold one', pageName: 'another page', lineNumber: 3}
+      ],
+      type: FormulaOutputType.NodeMarkdown
+    };
+  }],
 ];
