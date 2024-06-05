5df14febd7024091d1e445d381b06148c8991306
lukasb <lukasb@gmail.com>
2024-06-04 17:09:12 -0700

    placeholder page data should be markdown

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -10,7 +10,7 @@ const users = [
   const pages = [
     {
       id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-      value: `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Hello, world!","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`,
+      value: `- `,
       userId: '410544b2-4001-4271-9855-fec4b6a6442a',
       title: 'My First Page'
     }
