a0c094e72751840779c36baeed301b0b6ee75efc
lukasb <lukasb@gmail.com>
2024-06-19 15:17:12 -0700

    great googly moogly I think it kinda works

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./_app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
