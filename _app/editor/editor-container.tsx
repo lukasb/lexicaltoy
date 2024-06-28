a928f4b699bae63aa814ddcad2de5cdad06d6500
lukasb <lukasb@gmail.com>
2024-06-28 15:06:17 -0700

    close from three dots on touch devices

--- _app/editor/editor-container.tsx
+++ _app/editor/editor-container.tsx
@@ -56,7 +56,7 @@ function EditorContainer({
   return (
     <div className="flex flex-col items-start md:mb-4">
       <div className="relative border-solid shadow-md dark:shadow-gray-500/50 md:shadow-none md:border-4 md:border-indigo-300 md:rounded-lg m-0 pt-2 pr-2.5 md:pr-7 pb-7 pl-0 w-full max-w-7xl">
-        {!isMobile && (
+        {!isMobile && !touchDevice && (
           <button
             className="absolute top-2 left-2 text-base text-indigo-600 py-0 px-1 rounded md:opacity-0 md:hover:opacity-100 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-red-500 z-40"
             onClick={() => closePage(page.id)}
@@ -113,7 +113,7 @@ function EditorContainer({
                     align="end"
                     sideOffset={5}
                   >
-                    {isMobile && (
+                    {(isMobile || touchDevice) && (
                       <DropdownMenu.Item
                         className="text-sm px-3 py-2 outline-none cursor-pointer text-gray-200 hover:bg-gray-700"
                         onClick={() => closePage(page.id)}
