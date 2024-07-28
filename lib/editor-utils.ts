546823b63968f795fe2a82bf474f1886d2be6b68
lukasb <lukasb@gmail.com>
2024-07-28 12:01:09 -0700

    oops need this too

---
+++ lib/editor-utils.ts
@@ -0,0 +1,27 @@
+import { LexicalEditor } from "lexical";
+import { createHeadlessEditor } from '@lexical/headless';
+import { ListNode, ListItemNode } from "@lexical/list";
+import { LinkNode, AutoLinkNode } from "@lexical/link";
+import { WikilinkNode, WikilinkInternalNode } from "@/_app/nodes/WikilinkNode";
+import { TodoCheckboxStatusNode } from "@/_app/nodes/TodoNode";
+import { FormulaEditorNode, FormulaDisplayNode } from "@/_app/nodes/FormulaNode";
+import { FormattableTextNode } from "@/_app/nodes/FormattableTextNode";
+
+export function myCreateHeadlessEditor(): LexicalEditor {
+  const editor = createHeadlessEditor({
+    nodes: [
+      LinkNode,
+      ListNode,
+      ListItemNode,
+      AutoLinkNode,
+      WikilinkNode,
+      WikilinkInternalNode,
+      TodoCheckboxStatusNode,
+      FormulaEditorNode,
+      FormulaDisplayNode,
+      FormattableTextNode
+    ],
+    onError: console.error,
+  });
+  return editor;
+}
