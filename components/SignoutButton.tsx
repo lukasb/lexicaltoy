294d391f8136ec1373eea0b31451e1419e6ccc99
lukasb <lukasb@gmail.com>
2024-12-03 16:43:59 -0800

    okay

--- components/SignoutButton.tsx
+++ components/SignoutButton.tsx
@@ -1,19 +1,26 @@
 import { Button } from "@/_app/ui/button";
 import { signOut } from "next-auth/react";
 import { localDb } from "@/_app/context/storage/db";
+import { useState } from "react";
 
 export const SignoutButton = () => {
+  const [buttonText, setButtonText] = useState("Sign Out");
+
   const handleClick = async () => {
     try {
+      setButtonText("Clearing data...");
       await localDb.delete();
+      setButtonText("Signing out...");
+      signOut();
     } catch (e) {
       alert("Error deleting local db: " + e);
+      setButtonText("Sign Out");
     }
-    signOut();
   };
+
   return (
     <Button onClick={handleClick}>
-      <div>Sign Out</div>
+      <div>{buttonText}</div>
     </Button>
   );
 }
