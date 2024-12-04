84d8ce694a3edb6928785487bc3fb81ddc6519d6
lukasb <lukasb@gmail.com>
2024-12-03 16:22:32 -0800

    signing out clears local db

--- components/SignoutButton.tsx
+++ components/SignoutButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/_app/ui/button";
 import { signOut } from "next-auth/react";
+import { localDb } from "@/_app/context/storage/db";
 
 export const SignoutButton = () => {
   const handleClick = () => {
     signOut();
+    localDb.delete();
   };
   return (
     <Button onClick={handleClick}>
