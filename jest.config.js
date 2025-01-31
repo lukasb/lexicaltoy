f326e42af181f7abee3f1e3b97c3e51b0046983f
lukasb <lukasb@gmail.com>
2025-01-31 11:52:20 -0600

    can create jest tests for code using remark

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 // @ts-check
+const fs = require('fs/promises');
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+const baseConfig = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
@@ -24,3 +25,26 @@ module.exports = {
   modulePaths: ['<rootDir>'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
 };
+
+async function jestConfig() {
+  const modules = await fs.readdir("node_modules");
+  let esModules = [];
+
+  for (const m of modules) {
+    try {
+      await import(m);
+    } catch (error) {
+      esModules.push(m);
+    }
+  }
+
+  const esModulesPattern = esModules.join("|");
+  return {
+    ...baseConfig,
+    transformIgnorePatterns: [
+      `/node_modules/(?!${esModulesPattern}).+.(js|jsx|mjs|cjs|ts|tsx)$/`
+    ]
+  };
+}
+
+module.exports = jestConfig();
